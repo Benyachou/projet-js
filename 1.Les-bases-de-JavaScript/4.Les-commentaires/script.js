f11270afef80e2b05a380b82f0aa84695e58ee91
Yassine Benyachou <Ben.yachou@yahoo.com>
2025-01-30 16:28:10 +0100

    les bases

---
+++ 1.Les-bases-de-JavaScript/4.Les-commentaires/script.js
@@ -0,0 +1,17 @@
+/* Les commentaires sur une ligne */
+
+
+
+/* Les commentaires sur plusieurs lignes */
+
+
+const userName = "Jack";
+
+/* Raccourcis clavier */
+
+// A. ctrl + / ou ctrl + k + c/u pour commenter en une ligne.
+// B. shift + alt + a pour commenter sur plusieurs lignes.
+
+/* Tout est règlable dans les shortcuts de VS Code. */
+
+
