01910d6632968e978d3edfe7bfba58f9cd8d2d45
Yassine Benyachou <Ben.yachou@yahoo.com>
2025-02-06 13:04:10 +0100

    correction

--- 1.Les-bases-de-JavaScript/Exercices/3.Objets-tableaux-DOM/script.js
+++ 1.Les-bases-de-JavaScript/Exercices/3.Objets-tableaux-DOM/script.js
@@ -3,17 +3,34 @@
     Ex: name, price, doors, kilometers, maxSpeed, year, etc ...
 */
 
+const car = {
+    name: "Clio 3",
+    price: 2500,
+    année: 2008,
+    couleur: "Rouge"
+}
+
+console.log(car.couleur);
+
 
 /* 
     2. Vous disposez d'un tableau de fruits ci-dessous.
-    A - Loggez le troisième fruit dans la console.
+    A - Loggez le deuxieme fruit dans la console.
     B - Loggez le dernier fruit dans la console.
     C - Retournez la longueur du tableau.
 */
 
 const fruits = ["🍇","🍓","🍌"]
 
+console.log(fruits[1]);
+console.log(fruits [fruits.length - 1]);
+console.log(fruits.length);
+
+
 
 /* 
     3. Loggez l'objet global BOM.
 */
+
+console.log(window);
+
